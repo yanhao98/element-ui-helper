98e350545ec97fcba8448da617973f30d25ff952
严浩 <me@yanhao.ren>
2024-05-28 17:44:47 +0800

    修复 usePaginationElement 传入参数会影响 ...GLOBAL_CONFIG.hooks.usePaginationElement

--- src/components/hooks/usePaginationElement.ts
+++ src/components/hooks/usePaginationElement.ts
@@ -38,11 +38,12 @@ export function usePaginationElement<R, P extends unknown[] = any>(options: Pagi
           ...GLOBAL_CONFIG.hooks.usePaginationElement?.defaultParams,
         },
       ],
-      ...(omit(merge(GLOBAL_CONFIG.hooks.usePaginationElement, options), [
-        'onFetch',
-        'defaultParams',
-        'elPaginationAttrs',
-      ]) as any),
+      ...(
+        omit(
+          merge(options, GLOBAL_CONFIG.hooks.usePaginationElement),
+          ['onFetch', 'defaultParams', 'elPaginationAttrs']
+        ) as any
+      ),
 
       // 节流: n 秒内只运行一次，若在 n 秒内重复触发，只有一次生效
       // 防抖: n 秒后在执行该事件，若在 n 秒内被重复触发，则重新计时
